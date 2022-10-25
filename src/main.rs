52bc417ff26377774050516bb405572c30802d1b
kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-10-25 20:30:22 +0900

    :sparkles: Add port search

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use serialport;
+
 fn main() {
-    println!("Hello, world!");
+  let ports = serialport::available_ports().expect("No ports found!");
+  for (idx, port) in ports.iter().enumerate() {
+    println!("{}: {}", idx, port.port_name);
+  }
 }
